1bff5bb42dba95af8e25cdf80e53c386a3dd3f80
Pradhyankita Bhasme <pradhyankita@gmail.com>
2022-11-28 14:27:47 +0530

    Adding: Find maximum - Integer

--- src/com/bridgelabz/FindMaximum.java
+++ src/com/bridgelabz/FindMaximum.java
@@ -1,11 +1,16 @@
 package com.bridgelabz;
 
-public class FindMaximum<K extends Comparable> {
-
-    K getMaximum(K n1, K n2, K n3){
-        K max=n1;
-        if (n2.compareTo(max) > 0) max=n2;
-        if (n3.compareTo(max) > 0) max=n3;
-        return max;
-    }
+public class FindMaximum{
+    static int findMaximumInt(Integer n1, Integer n2, Integer n3){
+    Integer max=n1;
+    if (n2.compareTo(max) > 0) max=n2;
+    if (n3.compareTo(max) > 0) max=n3;
+    return max;
+}
+    public static void main(String[] args) {
+        int num1,num2,num3;
+        num1=5;num2=9;num3=7;
+        int maximum=findMaximumInt(num1,num2,num3);
+        System.out.println("maximum = "+maximum);
+        }
 }
